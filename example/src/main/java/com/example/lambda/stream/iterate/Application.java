fb10234e21184c0c83a53f17fb5b38c937d5bcc4
David Petro <davidpetro@MacBook-Air-de-David.local>
2018-05-31 18:12:01 -0300

    add example iterate

--- example/src/main/java/com/example/lambda/stream/iterate/Application.java
+++ example/src/main/java/com/example/lambda/stream/iterate/Application.java
@@ -5,8 +5,14 @@ import java.util.stream.Stream;
 public class Application {
 
   public static void main(String[] args) {
+    System.out.println("Example 1");
     Stream.iterate(1, x -> x + 1)
       .limit(3)
+      .forEach(System.out::println);
+
+    System.out.println("Example 2  With filter does not terminate!" );
+    Stream.iterate(1, x -> x + 1)
+      .filter(x -> x <=3 && x >0 )
       .forEach(System.out::println);
   }
 }
