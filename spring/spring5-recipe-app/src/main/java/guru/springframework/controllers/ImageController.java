e9d9b16491299c422a1494a56ccb4a01f6c6d541
David Petro <davidpetro@MacBook-Air-de-David.local>
2018-06-13 22:34:11 -0300

    add more example

--- spring/spring5-recipe-app/src/main/java/guru/springframework/controllers/ImageController.java
+++ spring/spring5-recipe-app/src/main/java/guru/springframework/controllers/ImageController.java
@@ -29,14 +29,14 @@ public class ImageController {
     }
 
     @GetMapping("recipe/{id}/image")
-    public String showUploadForm(@PathVariable String id, Model model) {
+    public String showUploadForm(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
 
         return "recipe/imageuploadform";
     }
 
     @PostMapping("recipe/{id}/image")
-    public String handleImagePost(@PathVariable String id, @RequestParam("imagefile") MultipartFile file) {
+    public String handleImagePost(@PathVariable String id, @RequestParam("imagefile") MultipartFile file){
 
         imageService.saveImageFile(Long.valueOf(id), file);
 
@@ -51,7 +51,7 @@ public class ImageController {
             byte[] byteArray = new byte[recipeCommand.getImage().length];
             int i = 0;
 
-            for (Byte wrappedByte : recipeCommand.getImage()) {
+            for (Byte wrappedByte : recipeCommand.getImage()){
                 byteArray[i++] = wrappedByte; //auto unboxing
             }
 
